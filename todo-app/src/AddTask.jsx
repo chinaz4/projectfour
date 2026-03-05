84b6f322cfa51843e986f46b444d7366022685f4
chinaz4 <chinaz4joy@gmail.com>
2026-03-05 13:49:45 +0000

    complete

--- todo-app/src/AddTask.jsx
+++ todo-app/src/AddTask.jsx
@@ -1,24 +1,46 @@
 import { useState } from "react";
 
-function AddTask({addNo}){
+function AddTask({addNo, onDelete}){
     const [name, setname] = useState("")
+     const [completed, setCompleted] = useState(false);
+
     return(
         <div className="bg-amber-950 rounded-2xl py-5 px-10 flex flex-col gap-y-4 text-[#F5F5DC] ">
         <h1 className="font-sans">
             Task {addNo}
         </h1>
 
-        <input  className=" text-2xl rounded-md w-auto h-12 font-sans"
+        <input  className={`text-2xl rounded-md w-auto h-12 font-sans ${
+          completed ? "line-through" : ""
+        }`}
         type="text"
         value= {name}
         onChange={(e) => setname(e.target.value)}
         placeholder="Enter task name" />
 
-        <button        
-        className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
-      >
-        Add Task
-      </button>      
+        <div className="flex gap-4">
+        <button className="bg-gray-500 text-white px-4 py-2 rounded  transform transition-transform duration-100
+    active:scale-90 ">
+          Add Task
+        </button>
+
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded  transform transition-transform duration-100
+    active:scale-90"
+          onClick={onDelete} 
+        >
+          Delete
+        </button>
+        <button
+          className={`bg-green-500 text-white px-4 py-2 rounded  transform transition-transform duration-100
+    active:scale-90 ${
+            completed ? "opacity-70" : ""
+          }`}
+          onClick={() => setCompleted(!completed)} 
+        >
+          {completed ? "Completed" : "Complete"}
+        </button>
+      </div>    
 
         </div>
     );
--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,25 +2,43 @@ import { useState } from "react";
 import AddTask from "./AddTask";
 
 function App() {
-  const [addTask, setaddTask] = useState(0);
+  const [tasks, setTasks] = useState([]);
+
 
 
 
   const CreateTask = () => {
-  setaddTask(prev => prev + 1);  
+      const newTask = {
+      id: Date.now() // unique id    
+    };
+
+  setTasks(prev =>[...prev, newTask] );  
 };
 
+     const deleteTask = (id) =>{
+      setTasks(prev => prev.filter(task => task.id !== id));
+     } 
+
+    
   return (
-    <div className="bg-[#F5F5DC] flex flex-col gap-2.5 p-5">
-      <button className="w-30 px-4 py-2 bg-blue-500" 
+      
+
+    <div className="bg-[#F5F5DC] flex flex-col gap-4 p-4 sm:p-6 md:p-8 lg:p-10 min-h-screen">
+      <h1 className="text-3xl text-amber-950 text-center p-10 font-bold">TODO APP</h1>
+      <button className=" sm:w-40 md:w-48 lg:w-56 px-4 py-2 bg-amber-950 text-[#F5F5DC] rounded-md " 
       onClick={CreateTask}   >
         create task
       </button>
-
-      {Array.from({ length: addTask }).map((_, index) => (
-  <AddTask key={index} addNo={index + 1} />
-))}
-
+      <div className="flex flex-col space-y-4">
+       {tasks.map((task, index) => (
+        <AddTask
+          key={task.id}
+          addNo={index + 1}
+          onDelete={() => deleteTask(task.id)}
+         
+        />
+      ))}
+      </div>
      
     </div>
   )
